30cfae42f0f04a51f895a19cc9ad2851a19b3340
Paulpoomrit <paulpoomrit@gmail.com>
2025-09-07 15:59:05 -0700

    Tile sprite variation added

--- tilegraphicsitem.cpp
+++ tilegraphicsitem.cpp
@@ -1,8 +1,25 @@
 #include "tilegraphicsitem.h"
 
+#include <qrandom.h>
+
 TileGraphicsItem::TileGraphicsItem(QGraphicsItem *parent) :
-    QGraphicsPixmapItem(parent)
+    QGraphicsPixmapItem(parent),
+    currentTileState(TileState::IDLE)
 {
-    QPixmap pixmap(":/tiles/Content/Tiles/tile_0000.png");
+    std::vector<QString> tileSprites = {":/tiles/Content/Tiles/tile_0000.png",
+                                        ":/tiles/Content/Tiles/tile_0001.png",
+                                        ":/tiles/Content/Tiles/tile_0002.png"};
+    int randomIndex = QRandomGenerator::global()->bounded(static_cast<int>(tileSprites.size()));
+    QPixmap pixmap(tileSprites.at(randomIndex));
     setPixmap(pixmap);
+}
+
+TileState TileGraphicsItem::getCurrentTileState() const
+{
+    return currentTileState;
+}
+
+void TileGraphicsItem::setCurrentTileState(TileState newCurrentTileState)
+{
+    currentTileState = newCurrentTileState;
 }
--- tilegraphicsitem.h
+++ tilegraphicsitem.h
@@ -2,11 +2,20 @@
 #define TILEGRAPHICSITEM_H
 
 #include <QGraphicsPixmapItem>
+#include "tilestates.h"
 
 class TileGraphicsItem : public QGraphicsPixmapItem
 {
 public:
     explicit TileGraphicsItem(QGraphicsItem *parent = nullptr);
+    TileState getCurrentTileState() const;
+    void setCurrentTileState(TileState newCurrentTileState);
+
+signals:
+    void tileStateChanged(TileState newState);
+
+private:
+    TileState currentTileState;
 };
 
 #endif // TILEGRAPHICSITEM_H
