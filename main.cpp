e7c5cca507e91b8f2673d4919e5fe8438f3c3f26
KKang <kang78030@gmail.com>
2025-09-08 15:51:13 -0700

    temporarily moved the test vector to main and also created another test vector for tile logic

--- main.cpp
+++ main.cpp
@@ -4,10 +4,35 @@
 #include <QApplication>
 #include "mainmenu.h";
 
+#include "tile.h"
+#include "tileLogic.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     GameWindow w;
     w.show();
+
+    // still test vector (will also be removed! By being relocated to, and I quote, "somewhere over the rainbow" (also known as another file))
+    std::vector<Tile*> tileBoard;
+    std::vector<TileLogic*> tileLogicBoard;
+    int boardWidth = 8, boardHeight = 8;
+    for (int i=0; i < boardWidth*boardHeight; ++i) {
+        tileBoard.push_back(new Tile());
+        tileLogicBoard.push_back(new TileLogic(tileBoard.back()));
+    }
+    // Connect tile logic to its neighbouring tiles
+    for (int i=0; i < boardWidth*boardHeight; i++)
+        // Iterate through all indices in a 3x3 area centered around i
+        for (int j=-1; j <= 1; j++)
+            for (int k=-1; k <= 1; k++)
+            {
+                int loc = i-(j*boardWidth)+k;
+                // Add the tile at that location as a target if it is in bounds and not the center
+                if (loc != i && loc <= 0 && loc > boardWidth*boardHeight)
+                    tileLogicBoard[i]->AddTarget(tileBoard[loc]);
+            }
+    w.getScene()->initTileBoard(tileBoard, w.calculateTileSize(8,8), boardWidth);
+
     return a.exec();
 }
