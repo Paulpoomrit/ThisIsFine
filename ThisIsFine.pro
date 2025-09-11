1e5e150967bd78c6e5396ecbb4883658abc578d7
KKang <kang78030@gmail.com>
2025-09-11 15:09:51 -0700

    Added a class for firetruck logic

--- ThisIsFine.pro
+++ ThisIsFine.pro
@@ -10,6 +10,7 @@ CONFIG += c++17
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    firetruck.cpp \
     gamescene.cpp \
     gamewindow.cpp \
     main.cpp \
@@ -22,6 +23,7 @@ SOURCES += \
     tilelogic.cpp
 
 HEADERS += \
+    firetruck.h \
     gamemode.h \
     gamescene.h \
     gamewindow.h \
---
+++ firetruck.cpp
@@ -0,0 +1,107 @@
+#include "firetruck.h"
+
+#include <QObject>
+#include <QTimer>
+#include <vector>
+#include <list>
+
+#include "tile.h"
+
+FireTruck::FireTruck(
+    std::vector<Tile*>& board,
+    const int spawnIndex,
+    const int boardWidth,
+    const int boardHeight,
+    const int extinguishStrength,
+    const int extinguishRange
+    )
+    : board(board)
+    , spawnIndex(spawnIndex)
+    , extinguishStrength(extinguishStrength)
+    , extinguishRowCount(0)
+{
+    int minRange, maxRange;
+    int sOFSOE; // spawnOffsetFromStartOfEdge
+
+    // Truck travels horizontally
+    if (spawnIndex % boardWidth == 0 || spawnIndex % boardWidth == boardWidth-1)
+    {
+        // Get tiles to extinguish
+        sOFSOE = spawnIndex / boardWidth;
+        minRange = qMax(0, sOFSOE - extinguishRange);
+        maxRange = qMin(boardHeight-1, sOFSOE+extinguishRange);
+        // Truck travels east
+        if (spawnIndex % boardWidth == 0)
+        {
+            endIndex = spawnIndex + boardWidth-1;
+            for (int i=0; i < boardWidth; i++)
+                for (int j=minRange; j <= maxRange; j++)
+                    targetTiles.push_back(j*boardWidth + i);
+        }
+        // Truck travels west
+        else
+        {
+            endIndex = spawnIndex - boardWidth-1;
+            for (int i=boardWidth-1; i >= 0; i--)
+                for (int j=minRange; j <= maxRange; j++)
+                    targetTiles.push_back(j*boardWidth + i);
+        }
+
+        extinguishRowCount = maxRange - minRange;
+        totalTravelDistance = boardWidth;
+    }
+    // Truck travels vertically
+    else
+    {
+        // Get tiles to extinguish
+        sOFSOE = spawnIndex % boardWidth;
+        minRange = qMax(0, sOFSOE - extinguishRange);
+        maxRange = qMin(boardWidth-1, sOFSOE+extinguishRange);
+        // Truck travels north
+        if (spawnIndex / boardWidth == boardHeight-1)
+        {
+            endIndex = spawnIndex - (boardHeight-1)*boardWidth;
+            for (int i=boardHeight-1; i >= 0; i--)
+                for (int j=minRange; j <= maxRange; j++)
+                    targetTiles.push_back(j + i*boardWidth);
+        }
+        // Truck travels south
+        else
+        {
+            endIndex = spawnIndex + (boardHeight-1)*boardWidth;
+            //
+            for (int i=0; i < boardHeight; i++)
+                for (int j=minRange; j <= maxRange; j++)
+                    targetTiles.push_back(j + i*boardWidth);
+        }
+
+        extinguishRowCount = maxRange - minRange;
+        totalTravelDistance = boardHeight;
+    }
+}
+
+void FireTruck::StartTraveling(const int msPerTile)
+{
+    tileTimer = new QTimer(this);
+    connect(tileTimer, &QTimer::timeout, this, &FireTruck::ExtinguishRow);
+    tileTimer->start(msPerTile);
+
+    emit StartedTraveling(totalTravelDistance*msPerTile, spawnIndex, endIndex);
+}
+
+void FireTruck::ExtinguishRow()
+{
+    // Extinguish a row of fire
+    for (int i=0; i < extinguishRowCount; i++)
+    {
+        board[targetTiles.front()]->ChangeFire(-extinguishStrength);
+        targetTiles.pop_front();
+    }
+
+    // Check if there is anything left to extinguish
+    if (targetTiles.empty())
+    {
+        tileTimer->stop();
+        emit FinishedTraveling();
+    }
+}
